c1cc50e1ad1762fb71fb11793eba813ef70c3911
Julien Vanelian <julienvanelian@hotmail.com>
2019-12-05 16:03:03 +0100

    Added loader

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -5,51 +5,84 @@ export default class Search extends React.Component {
         showHits: [],
         peopleHits: [],
         hasError: false,
+        fetchingShows: false,
+        fetchingPeople: false,
     };
 
     handleKeyDown = (e) => {
         if (e.key === 'Enter' && e.target.value !== '') {
+            this.setState({fetchingShows: true, fetchingPeople: true})
             this.setState({showHits: [], peopleHits: []})
             this.setState({hasError: false})
 
             fetch(' http://api.tvmaze.com/search/shows?q=' + e.target.value)
                 .then(response => response.json())
-                .then(data => this.setState({showHits: data}))
-                .catch(() => this.setState({hasError: true}))
+                .then(data => {
+                    this.setState({showHits: data})
+                    this.setState({fetchingShows: false})
+                })
+                .catch(() => {
+                    this.setState({hasError: true})
+                    this.setState({fetchingShows: false})
+                })
 
             fetch(' http://api.tvmaze.com/search/people?q=' + e.target.value)
                 .then(response => response.json())
-                .then(data => this.setState({peopleHits: data}))
-                .catch(() => this.setState({hasError: true}))
+                .then(data => {
+                    this.setState({peopleHits: data})
+                    this.setState({fetchingPeople: false})
+                })
+                .catch(() => {
+                    this.setState({hasError: true})
+                    this.setState({fetchingPeople: false})
+                })
         }
     }
 
     render() {
-        const {showHits, peopleHits} = this.state;
+        const {showHits, peopleHits, fetchingShows, fetchingPeople} = this.state;
 
         return (
             <>
                 <input type="text" placeholder="Search your favorite TV show" className="input-search"
                        onKeyDown={this.handleKeyDown}/>
 
-                <h1>TV Shows</h1>
-                <ul>
-                    {showHits.map(hit =>
-                        <li key={hit.show.id}>
-                            <a href={hit.show.url}>{hit.show.name}</a>
-                        </li>
-                    )}
-                </ul>
+                {(fetchingShows || fetchingPeople) &&
+                <>
+                    <div className="lds-ring">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                </>
+                }
 
-                <h1>People</h1>
-                <ul>
-                    {peopleHits.map(hit =>
-                        <li key={hit.person.id}>
-                            <a href={hit.person.url}>{hit.person.name}</a>
-                        </li>
-                    )}
-                </ul>
+                {showHits.length > 0 &&
+                <div>
+                    <h1>TV Shows</h1>
+                    <ul>
+                        {showHits.map(hit =>
+                            <li key={hit.show.id}>
+                                <a href={hit.show.url}>{hit.show.name}</a>
+                            </li>
+                        )}
+                    </ul>
+                </div>
+                }
 
+                {peopleHits.length > 0 &&
+                <div>
+                    <h1>People</h1>
+                    <ul>
+                        {peopleHits.map(hit =>
+                            <li key={hit.person.id}>
+                                <a href={hit.person.url}>{hit.person.name}</a>
+                            </li>
+                        )}
+                    </ul>
+                </div>
+                }
             </>
         )
     }
